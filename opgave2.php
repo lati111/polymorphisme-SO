690d39acbb1f3661ba9fefb175b5eafcf221d25b
lati111 <jvzelst111@gmail.com>
2022-09-20 12:29:35 +0200

    opgave 2 added doctype support

--- opgave2.php
+++ opgave2.php
@@ -2,10 +2,12 @@
 interface IDocument
 {
     public function getNaam();
+    public function getDocType();
 }
 class TextDocument implements IDocument
 {
     private $name;
+    private $docType = "Text document";
     //Specifieke properties voor een textdoc weggelaten 
 
     public function __construct(string $name)
@@ -17,12 +19,18 @@ class TextDocument implements IDocument
     {
         return $this->name;
     }
+
+    public function getDocType()
+    {
+        return $this->docType;
+    }
 }
 
 
 class Worksheet implements IDocument
 {
     private $name;
+    private $docType = "Worksheet";
     //Specifieke properties voor een worksheet weggelaten 
 
     public function __construct(string $name)
@@ -34,12 +42,18 @@ class Worksheet implements IDocument
     {
         return $this->name;
     }
+
+    public function getDocType()
+    {
+        return $this->docType;
+    }
 }
 
 
 class Drawing implements IDocument
 {
     private $name;
+    private $docType = "Drawing";
     //Specifieke properties voor een drawing weggelaten 
 
     public function __construct(string $name)
@@ -51,6 +65,10 @@ class Drawing implements IDocument
     {
         return $this->name;
     }
+
+    public function getDocType() {
+        return $this->docType;
+    }
 }
 
 
@@ -60,6 +78,5 @@ $boekhouding2021 = new Worksheet('Boekhouding Bedrijf X 20202021');
 $grafiekresult = new Drawing('Resultaten boekjaar 2019 - 2020');
 $bedrijfdocs = array($handleiding, $boekhouding1920, $boekhouding2021, $grafiekresult);
 foreach ($bedrijfdocs as $doc) {
-    echo $doc->getNaam() . "<br>";
-    // Wat gebeurt hier? Goed over nadenken! 
+    echo $doc->getNaam() . " (" .  $doc->getDocType() . ")<br>";
 }
